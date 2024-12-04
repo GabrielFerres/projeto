063b38f41eea1d3492cbf500067d8fc1a338e9d3
Gabriel Ferres <bielferres@gmail.com>
2024-12-04 11:18:22 -0300

    change img light mode

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function toggleMode () {
     const html = document.documentElement
+    html.classList.toggle('light');
 
-    html.classList.toggle('light')
+    const img = document.querySelector('#profile img');
     
-    // if(html.classList.contains('light')){
-    //     html.classList.remove('light')
-    // } else {
-    //     html.classList.add('light')
-    // }
+    if(html.classList.contains('light')){
+       img.setAttribute('src', './assets/avatar.png'); //colocar png no modo light, com osculos de sol
+    } else {
+        img.setAttribute('src', './assets/avatar.png')
+    }
 
     
 }
